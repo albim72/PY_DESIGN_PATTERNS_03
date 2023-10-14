8e626b36411aee5bf47d7162ac1d8653c8b79356
albim72 <marcin.albiniak72@gmail.com>
2023-10-14 15:41:59 +0200

    Create asyncurl.py

---
+++ asyncurl.py
@@ -0,0 +1,11 @@
+import  concurrent.futures
+import urllib.request
+
+URLS = [
+    "https://www.foxnews.com/",
+    "https://edition.cnn.com/",
+    "https://www.bbc.co.uk/",
+    "https://www.wp.pl/",
+    "https: // www.gov.pl / web / obrona - narodowa",
+    "https://mil.ru/"
+]
