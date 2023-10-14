fe86bdec27502ac406e5854323d7ed8242baf73d
albim72 <marcin.albiniak72@gmail.com>
2023-10-14 12:43:07 +0200

    Update iteratory.py

--- iteratory.py
+++ iteratory.py
@@ -24,22 +24,41 @@ class Employee:
     fisrtname:str
     lastname:str
     accepted:bool = False
-    
-    
+
+
 @dataclass
 class Company:
     name:str
-    
+
     def __post_init__(self):
         self._employees:List[Employee] = []
 
     def hire_employee(self,employee:Employee):
         self._employees.append(employee)
-        
+
     def fire_employee(self,employee:Employee):
         self._employees.remove(employee)
-        
+
     def __iter__(self):
         return iter(filter(lambda x:x.accepted, self._employees))
-    
-    
+
+first_employee = Employee('Paweł','Kot',True)
+second_employee = Employee('Olga','Nowak',True)
+third_employee = Employee('Olaf','Kowal')
+
+
+company = Company('ABC')
+company.hire_employee(first_employee)
+company.hire_employee(second_employee)
+company.hire_employee(third_employee)
+
+for emp in company:
+    print(emp)
+
+
+company.fire_employee(first_employee)
+
+print("usunięcie rekordu...")
+
+for emp in company:
+    print(emp)
