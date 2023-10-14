3fb60c16c2128da3f140cef8b1f8dbe02e4966a8
albim72 <marcin.albiniak72@gmail.com>
2023-10-14 10:12:55 +0200

    Update funkcja_w_funkcji.py

--- funkcja_w_funkcji.py
+++ funkcja_w_funkcji.py
@@ -14,3 +14,13 @@ def osoba(funkcja,*args):
 print(osoba(witaj,"Leon"))
 print(osoba(egzamin,"Olga",67,True))
 print(osoba(fx,4,7,2))
+
+#przykład 2
+liczba = [4,5,24,12,778,-5,0,-12,455,67,99,100,3,8,-23,22]
+
+parzyste = list(filter(lambda x:x%2==0,liczba))
+
+print(parzyste)
+
+cube = list(map(lambda a:a**3,liczba))
+print(cube)
