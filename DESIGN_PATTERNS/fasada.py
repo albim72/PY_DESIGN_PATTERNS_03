81c73b7648d03312e04ccc42b8bd3ad58c8272bd
albim72 <marcin.albiniak72@gmail.com>
2023-10-15 12:55:04 +0200

    Create fasada.py

---
+++ DESIGN_PATTERNS/fasada.py
@@ -0,0 +1,65 @@
+from enum import Enum
+from abc import ABCMeta,abstractmethod
+
+State = Enum('State','new running sleeping restart zombie')
+
+class User:
+    pass
+
+class Process:
+    pass
+
+class File:
+    pass
+
+class Server(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return self.name
+
+    @abstractmethod
+    def boot(self):
+        pass
+
+
+    @abstractmethod
+    def kill(self,restart=True):
+        pass
+
+
+class FileServer(Server):
+    def __init__(self):
+        self.name = "FileServer"
+        self.state = State.new
+
+    def boot(self):
+        print(f"Bootowanie systemu {self}")
+        self.state = State.running
+
+    def kill(self, restart=True):
+        print(f"Usuwanie {self}")
+        self.state = State.restart if restart else State.zombie
+
+    def create_file(self,user,name,permission):
+        print(f"próba utworzenia pliku: {name} dla użytkownika: {user} z uprawnieniami: {permission}")
+
+
+class ProcessServer(Server):
+    def __init__(self):
+        self.name = "ProcessServer"
+        self.state = State.new
+
+    def boot(self):
+        print(f"Bootowanie systemu {self}")
+        self.state = State.running
+
+    def kill(self, restart=True):
+        print(f"Usuwanie {self}")
+        self.state = State.restart if restart else State.zombie
+
+    def create_process(self, user, name):
+        print(f"próba utworzenia procesu: {name} dla użytkownika: {user}")
+
