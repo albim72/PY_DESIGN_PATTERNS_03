00ac26dfbfff37d3451f1aef2fdb5ca3c8bdde39
albim72 <marcin.albiniak72@gmail.com>
2023-10-15 14:28:11 +0200

    Update interpreter.py

--- DESIGN_PATTERNS/interpreter.py
+++ DESIGN_PATTERNS/interpreter.py
@@ -3,15 +3,15 @@ from pyparsing import Word,OneOrMore,Optional,Group,Suppress,alphanums
 class Gate:
     def __init__(self):
         self.is_open = False
-        
+
     def __str__(self):
         return 'open' if self.is_open else 'closed'
-    
+
     def open(self):
         print('opening the gate')
         self.is_open = True
-        
-    
+
+
     def close(self):
         print('closing the gate')
         self.is_open = False
@@ -31,14 +31,14 @@ class Garage:
     def close(self):
         print('closing the garage')
         self.is_open = False
-        
-        
+
+
 class Aircondition:
     def __init__(self):
         self.is_on = False
 
     def __str__(self):
-        return 'on' if self.is_open else 'off'
+        return 'on' if self.is_on else 'off'
 
     def turn_on(self):
         print('turning on the air condition')
@@ -47,3 +47,50 @@ class Aircondition:
     def turn_off(self):
         print('turning off the air condition')
         self.is_on = False
+
+
+class Heating:
+    def __init__(self):
+        self.is_on = False
+
+    def __str__(self):
+        return 'on' if self.is_on else 'off'
+
+    def turn_on(self):
+        print('turning on the air heating')
+        self.is_on = True
+
+    def turn_off(self):
+        print('turning off the air heating')
+        self.is_on = False
+        
+class Boiler:
+    def __init__(self):
+        self.temperature = 47
+
+    def __str__(self):
+        return f'boiler temperature: {self.temperature} C'
+
+    def increase_temperature(self,amount):
+        print(f"increasing the boiler's temperature by {amount} degrees")
+        self.temperature += amount
+
+    def decrease_temperature(self, amount):
+        print(f"decreasing the boiler's temperature by {amount} degrees")
+        self.temperature -= amount
+        
+        
+class Fridge:
+    def __init__(self):
+        self.temperature = 2
+
+    def __str__(self):
+        return f'fridge temperature: {self.temperature} C'
+
+    def increase_temperature(self,amount):
+        print(f"increasing the fridges's temperature by {amount} degrees")
+        self.temperature += amount
+
+    def decrease_temperature(self, amount):
+        print(f"decreasing the fridges's temperature by {amount} degrees")
+        self.temperature -= amount
