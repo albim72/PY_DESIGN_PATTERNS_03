d908171e5f47d21bff55748454e29b634c1644a7
albim72 <marcin.albiniak72@gmail.com>
2023-10-15 11:30:08 +0200

    Update pizza_builder.py

--- DESIGN_PATTERNS/pizza_builder.py
+++ DESIGN_PATTERNS/pizza_builder.py
@@ -91,3 +91,20 @@ class CreamyBaconBuilder:
         print(f'baking Your Creamy Bacon for {self.baking_time} s')
         time.sleep(self.baking_time)
         print('Your Creamy Bacon is ready!!!')
+        
+        
+class Waiter:
+    def __init__(self):
+        self.builder = None
+        
+    def construct_pizza(self,builder):
+        self.builder = builder
+        steps = (builder.prepare_dough,
+                 builder.add_sauce,
+                 builder.add_topping,
+                 builder.bake)
+        [step() for step in steps]
+        
+    @property
+    def pizza(self):
+        return self.builder.pizza
