1c10093ddb12a92460eea67a34f74a7d2fc76c24
albim72 <marcin.albiniak72@gmail.com>
2023-10-15 15:35:30 +0200

    Update bridge.py

--- DESIGN_PATTERNS/bridge.py
+++ DESIGN_PATTERNS/bridge.py
@@ -6,16 +6,16 @@ import urllib.parse
 class ResourceContent:
     def __init__(self,imp):
         self._imp = imp
-        
+
     def show_content(self,path):
         self._imp.fetch(path)
-        
+
 class ResourceContentFetcher(metaclass=ABCMeta):
     @abstractmethod
     def fetch(self,path):
         pass
-    
-    
+
+
 class URLFetcher(ResourceContentFetcher):
     def fetch(self, path):
         req = urllib.request.Request(path)
@@ -23,8 +23,22 @@ class URLFetcher(ResourceContentFetcher):
             if response.code == 200:
                 this_page = response.read()
                 print(this_page)
-                
+
 class LocalFileFetcher(ResourceContentFetcher):
     def fetch(self, path):
         with open(path,encoding='utf-8') as f:
             print(f.read())
+
+def main():
+    url_fetcher = URLFetcher()
+    iface = ResourceContent(url_fetcher)
+    iface.show_content('http://python.org')
+
+    print("_"*30)
+
+    locals_fetcher = LocalFileFetcher()
+    iface = ResourceContent(locals_fetcher)
+    iface.show_content('info.txt')
+
+if __name__ == '__main__':
+    main()
