0caf5a0615c2d1ddb1c8554cb2a93d6109a56a54
albim72 <marcin.albiniak72@gmail.com>
2023-10-15 16:27:00 +0200

    Update wzorzec_mvc.py

--- DESIGN_PATTERNS/wzorzec_mvc.py
+++ DESIGN_PATTERNS/wzorzec_mvc.py
@@ -5,3 +5,51 @@ cytaty = (
     'Pozbierać jest dzisięć razy trudniej niż rozsypać',
     'Kiedy łamiesz zasady, łam je mocno i na dobre'
 )
+
+class QuoteModel:
+    def get_quote(self,n):
+        try:
+            value = cytaty[n]
+        except IndexError as err:
+            print(f'nie znaleziono: {err}')
+        else:
+            return value
+
+
+class QuoteTerminalView:
+    def show(self,quote):
+        print(f'wybrany cytat: "{quote}"')
+
+    def error(self,msg):
+        print(f'Błąd: {msg}')
+
+    def select_quote(self):
+        return input('Któy numer cytatu wybierasz? ')
+
+class QuoteTerminalController:
+    def __init__(self):
+        self.model = QuoteModel()
+        self.view = QuoteTerminalView()
+
+    def run(self):
+        valid_input = False
+        while not valid_input:
+            try:
+                n = self.view.select_quote()
+                n = int(n)
+                valid_input = True
+            except ValueError as ve:
+                self.view.error(f'niewłaściwy index -> {n}')
+            quote = self.model.get_quote(n)
+            self.view.show(quote)
+
+def main():
+    controller = QuoteTerminalController()
+    while True:
+        controller.run()
+
+
+if __name__ == '__main__':
+    main()
+
+
