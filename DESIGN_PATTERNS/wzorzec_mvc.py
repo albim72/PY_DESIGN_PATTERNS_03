4385b6b7650d051d162d9aee0fe1884130fc80c2
albim72 <marcin.albiniak72@gmail.com>
2023-10-15 16:15:35 +0200

    Create wzorzec_mvc.py

---
+++ DESIGN_PATTERNS/wzorzec_mvc.py
@@ -0,0 +1,7 @@
+cytaty = (
+    'Lepiej zaliczać się do niektórych, niż do wszystkich',
+    'Książki są lustrem, widzisz w nich tylko to, co już masz w sobie',
+    'Życie jest jak pudełko czekoladek - nigdy nie wiesz, co Cię trafi',
+    'Pozbierać jest dzisięć razy trudniej niż rozsypać',
+    'Kiedy łamiesz zasady, łam je mocno i na dobre'
+)
