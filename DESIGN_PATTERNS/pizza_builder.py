6ff606326a49ae886ac81e898224fa30b1156e13
albim72 <marcin.albiniak72@gmail.com>
2023-10-15 10:45:40 +0200

    Create pizza_builder.py

---
+++ DESIGN_PATTERNS/pizza_builder.py
@@ -0,0 +1,26 @@
+from enum import Enum
+import time
+
+
+PizzaProgress = Enum('PizzaProgress','queue preparation baking ready')
+PizzaDough = Enum('PizzaDough','thin thick')
+PizzaSouce = Enum('PizzaSouce','tomato garlic')
+PizzaTopping = Enum('PizzaTopping','mozarella double_mozarella bacon ham mushrooms red_onion oregano')
+STEP_DELAY = 3
+
+
+class Pizza:
+    def __init__(self,name):
+        self.name = name
+        self.dough = None
+        self.sauce = None
+        self.topping = []
+        
+    def __str__(self):
+        return self.name
+    
+    def prepare_dough(self,dough):
+        self.dough = dough
+        print(f'prepairing the {self.dough.name} dough of your {self}')
+        time.sleep(STEP_DELAY)
+        print(f'done with the {self.dough.name} dough')
